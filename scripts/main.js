6783839fe4de625a3d7b6098e745cde071ab648e
doughertyg <doughertyg37@gmail.com>
2020-08-07 16:11:26 -0700

    add the modal generator

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById('menu-btn');
 const menuItemsWrapper = document.getElementsByClassName('--menu-items__wrapper');
+const viewer = document.getElementsByClassName('main__viewer')[0];
 
 function toggleMenu() {
   if ([...menuItemsWrapper[0].classList].includes('collapsed')) {
@@ -9,15 +10,29 @@ function toggleMenu() {
   }
 }
 
-function fetchData() {
+function fetchData(fn) {
   const req = new XMLHttpRequest();
   req.open('GET', '/data/grahamdougherty.json', true);
   req.onload = function() {
     const data = JSON.parse(req.responseText);
-    console.log('what is res? ', data);
+    fn(data);
   }
   req.send();
 }
 
-fetchData();
+function generateDataView(data) {
+  data.forEach(entry => {
+    const element = document.createElement('div');
+    element.className = "viewer__modal module";
+    element.innerHTML = `
+      <h1 class="experience-modal__company">${entry.company}</h1>
+      <h2 class="experience-modal__title">${entry.title}</h2>
+      <p class="experience-modal__date">${entry.date}</p>
+      <p class="experience-modal__desc">${entry.description}</p>
+    `
+    viewer.appendChild(element);
+  });
+}
+
+fetchData(generateDataView);
 menuButton.addEventListener('click', toggleMenu);
