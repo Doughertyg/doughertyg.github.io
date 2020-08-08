f62fc22d1555406c020b9884a86a8666c7ea4a90
doughertyg <doughertyg37@gmail.com>
2020-08-07 18:23:17 -0700

    add styling and flip click handler

--- scripts/main.js
+++ scripts/main.js
@@ -24,13 +24,19 @@ function generateDataView(data) {
   data.forEach(entry => {
     const element = document.createElement('div');
     element.className = "viewer__modal module";
-    element.onclick = function() {console.log('this? ', this);};
+    element.onclick = function() {
+      if ([...this.classList].includes('--flipped')) {
+        this.classList.remove('--flipped');
+      } else {
+        this.classList.add('--flipped');
+      }
+    };
     element.innerHTML = `
       <h1 class="experience-modal__title">${entry.title}</h1>
       <h2 class="experience-modal__company">${entry.company}</h2>
       <p class="experience-modal__desc">${entry.description}</p>
       <p class="experience-modal__date">${entry.date}</p>
-      <p class="experience-modal__work">${entry.work}</p>
+      <div class="experience-modal__work">${entry.work}</div>
     `
     viewer.appendChild(element);
   });
