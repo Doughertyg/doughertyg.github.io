c12410c6afda53409b821c31a98c0d4b2bb885a0
doughertyg <doughertyg37@gmail.com>
2020-08-07 19:08:17 -0700

    update data and height transition

--- scripts/main.js
+++ scripts/main.js
@@ -24,20 +24,34 @@ function generateDataView(data) {
   data.forEach(entry => {
     const element = document.createElement('div');
     element.className = "viewer__modal module";
-    element.onclick = function() {
-      if ([...this.classList].includes('--flipped')) {
-        this.classList.remove('--flipped');
-      } else {
-        this.classList.add('--flipped');
-      }
-    };
-    element.innerHTML = `
+    const html = `
       <h1 class="experience-modal__title">${entry.title}</h1>
       <h2 class="experience-modal__company">${entry.company}</h2>
       <p class="experience-modal__desc">${entry.description}</p>
       <p class="experience-modal__date">${entry.date}</p>
       <div class="experience-modal__work">${entry.work}</div>
     `
+    element.onclick = function() {
+      if ([...this.classList].includes('--flipped')) {
+        this.classList.remove('--flipped');
+        this.classList.add('--flipped-back');
+      } else {
+        this.classList.add('--flipped');
+        this.classList.remove('--flipped-back');
+      }
+    };
+
+    element.onanimationstart = function() {
+      setTimeout(function() {
+        if ([...element.classList].includes('--flipped')) {
+          element.innerHTML = `<p class="work-desc"><ul><li>${entry.work.split('\u2022 ').slice(1).join('</li><li>')}</li></ul></p>`
+        } else {
+          element.innerHTML = html;
+        }
+      }, 250);
+    };
+
+    element.innerHTML = html;
     viewer.appendChild(element);
   });
 }
